ba028b2184a583ae2e5f4a95ab54f07fbee2c42a
larnik35 <larnik35@mail.ru>
2022-12-01 15:27:09 +0300

    add . I have completed Task_6

--- ДЗ1/Task_6/Program.cs
+++ ДЗ1/Task_6/Program.cs
@@ -3,7 +3,7 @@ Console.Clear();
 Console.WriteLine("Введите число");
 double a = Convert.ToDouble(Console.ReadLine());
 double d = 0;
-if (a !=0 )
+if (a !=0 ) // Проверка на 0
   {d = a%2;
   if (d == 0) Console.WriteLine((a) + " - Число четное ");
   else Console.WriteLine((a) +" - Число не четное");
