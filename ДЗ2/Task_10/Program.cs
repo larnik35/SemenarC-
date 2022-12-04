6b146919b8ebbeee9199da88fb2874727a419176
larnik35 <larnik35@mail.ru>
2022-12-04 17:16:19 +0300

    add Task_10

--- ДЗ2/Task_10/Program.cs
+++ ДЗ2/Task_10/Program.cs
@@ -2,6 +2,12 @@
 Console.Clear();
 Console.WriteLine("Введите трехзначное число");
 int namber = int.Parse(Console.ReadLine());
+//bool isParse = int.TryParse(Console.ReadLine(), out int namber);
+//if (isParse == false)
+//{
+  //  Console.WriteLine("Ошибка");
+    //return;
+//} else {
 double namber2 = (namber%100 - namber%10)/10;
 Console.WriteLine("Вторая цифра числа  - " + namber2);
-
+//}
