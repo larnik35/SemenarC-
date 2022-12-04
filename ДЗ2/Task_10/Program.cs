01a167ae78a2dcaee5963f0a6a66b717d97dbe98
larnik35 <larnik35@mail.ru>
2022-12-04 16:38:04 +0300

    add Task_10

---
+++ ДЗ2/Task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int namber = int.Parse(Console.ReadLine());
+double namber2 = (namber%100 - namber%10)/10;
+Console.WriteLine("Вторая цифра числа  - " + namber2);
+
